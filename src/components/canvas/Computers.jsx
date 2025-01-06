57602b0e49e68f3555b47db6a66757efa55a2704
Laibah-Shahid <laibahshahid18@gmail.com>
2025-01-06 20:03:10 +0500

    initial commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,7 +12,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={13} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop='always'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
